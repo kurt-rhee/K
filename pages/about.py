4318fbdd2f2f33d883e8c565d8f8585959c031e5
Kurt Rhee <33131958+kurt-rhee@users.noreply.github.com>
2021-08-08 08:48:18 -0400

    Update about.py

--- pages/about.py
+++ pages/about.py
@@ -6,21 +6,38 @@ from diagrams import employment_history
 def build_about_page():
     """
     """
-    # --- header ---
-    st.write('## Employment History')
+    # --- about ---
+    st.write('## About Me')
+    st.write("""
+    Hello, my name is Kurt Rhee and I am a software developer and solar performance engineer, interested
+    in the intersection between the two disciplines.  My educational background is in NanoEngineering, but I 
+    taught myself how to program in Python in 2014 and I haven't stopped programming since.  
+    
+    Sankey diagrams located on each page throughout this portfolio display core competencies and libraries that I
+    have actively used to create some body of work.  On each project page, the edges highlighted in green show which
+    competencies I used on that specific project. 
+    
+    Outside of my main body of work, I am a part time stunt man, surfer, rock climber, environmentalist and bibliophile.
+    
+    Nice to meet you, and please feel free to contact me directly regarding any of the projects you may
+    want to talk about in greater detail.
+    """)
 
     # --- GANTT ---
+    st.write('## Employment History')
     gantt = employment_history()
     st.plotly_chart(gantt, use_container_width=True)
 
-
+    # --- columns ---
     c1, c2 = st.columns([2, 3])
+
     # --- left column ---
     c1.write('## Logistics')
     c2.write('## Other Stuff')
     me1 = {
         'Pronouns': ['he', 'him', 'his'],
         'Locations': ['San Diego County, CA', 'Remote'],
+        'Education': ['B.Sc. NanoEngineering from UCSD', 'Minor in Literature from UCSD'],
         'Contact': [
             'kurtrhee@gmail',
             r'https://www.linkedin.com/in/simonkurtisrhee/',
